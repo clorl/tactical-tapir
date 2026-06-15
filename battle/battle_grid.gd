2acdaf3ea658eac6adeb0faa793df421365f575a
Clorl <none@null.com>
2026-06-15 18:50:42 +0200

    Complicated but promising wip

--- battle/battle_grid.gd
+++ battle/battle_grid.gd
@@ -2,257 +2,189 @@
 extends Node3D
 class_name BattleGrid
 
-const DBG_POINT_SIZE := 0.1
+class CellInfo extends RefCounted:
+	var neighbors := 6
 
-@export var base_neighbor_count := 6:
-	set(v):
-		base_neighbor_count = v
-		generate()
-var modifiers: Array[GraphModifier]
-var mod_map := {}
+	var neighbor_overrides = {}
+	var vertices_overrides = {}
 
-var vertices: PackedVector2Array
+class Grid extends RefCounted:
+	var size: Vector2
+	var default_cell: CellInfo
 
+	var grid_to_world: Transform2D
+	var world_to_grid: Transform2D
+
+	var cell_overrides: Dictionary[Vector2, CellInfo]
+
+	func _init(n: int):
+		default_cell = CellInfo.new()
+		default_cell.neighbors = n
+
+		var angle = (2.0*PI)/n
+		var dist = cos(angle / 2.0) * 2.0
+		grid_to_world.x = Vector2.RIGHT * dist
+		grid_to_world.y = Vector2.RIGHT.rotated(angle).normalized() * dist
+		world_to_grid = grid_to_world.affine_inverse()
+
+	func _compute_overrides():
+		var new_overrides = cell_overrides
+
+		for pos in cell_overrides.keys():
+			var cell = cell_overrides[pos]
+			var neighbors = get_neighbors(pos, default_cell)
+			var square_vertices = get_vertices(pos)
+			var hex_vertices = [0, 1, 3, 4]
+			for i in range(hex_vertices.size()):
+				var square_vertex = square_vertices[i]
+				var hex_vindex = hex_vertices[i]
+				var override_map = get_vertex_index_for_neighbors(pos, hex_vindex, default_cell)
+				for neigh_idx in override_map.keys():
+					var neigh = neighbors[neigh_idx]
+					var new_vertex = override_map[neigh_idx]
+					var c = CellInfo.new()
+					c.vertices_overrides = {}
+					c.vertices_overrides.set(new_vertex, square_vertex)
+					print(neigh)
+					print(c.vertices_overrides)
+					print(override_map)
+					print(" ")
+					new_overrides.set(neigh, c)
+					
+		cell_overrides = new_overrides
+					
+
+	func get_vertex_index_for_neighbors(pos: Vector2, vertex_idx: int, cell = null) -> Dictionary:
+		cell = get_cell_info(pos, cell)
+		vertex_idx = posmod(vertex_idx, cell.neighbors)
+		var res = {}
+		res[posmod(vertex_idx, cell.neighbors)] = posmod(vertex_idx + 4, cell.neighbors)
+		res[posmod(vertex_idx - 1, cell.neighbors)] = posmod(vertex_idx + 2, cell.neighbors)
+		return res
+
+	func get_vertices(pos: Vector2, cell = null) -> PackedVector2Array:
+		pos = grid_to_world * pos
+		cell = get_cell_info(pos, cell)
+		var v = []
+		var angle = (2.0*PI)/cell.neighbors
+		var start_angle = -angle/2.0
+		var start_vector = Vector2.RIGHT.rotated(start_angle)
+		for i in range(cell.neighbors):
+			v.append(pos + start_vector.rotated(angle * i))
+		
+		for idx in cell.vertices_overrides.keys():
+			v[idx] = cell.vertices_overrides[idx]
+			
+		return v
+
+	func get_neighbors(pos: Vector2, cell = null) -> PackedVector2Array:
+		cell = get_cell_info(pos, cell)
+		assert(cell.neighbors == 4 or cell.neighbors == 6, "Only works for squares and hexes now")
+		if cell.neighbors == 4:
+			return [
+				pos + Vector2(1, 0),
+				pos + Vector2(0, 1),
+				pos + Vector2(-1, 0),
+				pos + Vector2(0, -1)
+			]
+		else:
+			return [
+				pos + Vector2(1, 0),
+				pos + Vector2(0, 1),
+				pos + Vector2(-1, 1),
+				pos + Vector2(-1, 0),
+				pos + Vector2(0, -1),
+				pos + Vector2(1, -1)
+			]
+
+	func get_cell_info(pos: Vector2, fallback = null) -> CellInfo:
+		if fallback != null and fallback is CellInfo:
+			return fallback
+		var c = cell_overrides.get(pos)
+		return c if c != null else default_cell
+		
+		
+
+@export_tool_button("Generate") var _generate_btn = generate
+
+@export var default_neighbor_count := 6:
+	set(v): default_neighbor_count = v; generate()
+@export var grid_size := Vector2i(6,6):
+	set(v): grid_size = v; generate()
+
+var grid_aabb: AABB
 var grid: Grid
 
-@export_group("Debug")
-@export var draw_vertices := true
-@export var preview_cursor := -1
+var modifiers: Dictionary[Vector2, GraphModifier]
 
-var once = false
-var _ts = 0
-var _generate_duration = 0
-var _first_frame_duration = 0
+#### MAIN LOGIC
+
 func generate():
-	once = false
-	_ts = Time.get_ticks_msec()
-	_generate_duration = 0
-	_first_frame_duration = 0
-	grid = Grid.new()
-	grid.neighbor_count = base_neighbor_count
 	modifiers.clear()
-	vertices.clear()
-	
+
+	grid = Grid.new(default_neighbor_count)
+	grid.size = Vector2(grid_size) * 2
+	grid_aabb = AABB(Vector3(-grid_size.x, 0, -grid_size.y), Vector3(grid_size.x * 2, .1, grid_size.y * 2))
+
 	for c in get_children():
-		if c is GraphModifier:
-			modifiers.append(c)
+		if not c is GraphModifier: continue
+		var pos_2d = grid.world_to_grid * Vector2(c.position.x, -c.position.z)
+		pos_2d.x = round(pos_2d.x)
+		pos_2d.y = round(pos_2d.y)
+		var cell_info = CellInfo.new()
+		cell_info.neighbors = c.neighbors
+		grid.cell_overrides.set(pos_2d, cell_info)
+		pos_2d = grid.grid_to_world * pos_2d
+		modifiers.set(pos_2d, c)
+	# var test = CellInfo.new()
+	# test.vertices_overrides = {
+	# 	2: Vector2(2, 2)
+	# }
+	#grid.cell_overrides.set(Vector2(0, 0), test)
+	grid._compute_overrides()
 
-	var cell = add_cell(Vector2(0,0))
-	var neighbors = grid.get_default_neighbors(Vector2(0,0))
-	for pos in neighbors:
-		grid.add_cell(pos)
-	vertices = grid.get_vertices()
-	
-	_generate_duration = Time.get_ticks_msec() - _ts
-	_ts = Time.get_ticks_msec()
-
-func add_cell(pos: Vector2) -> Cell:
-	var mod = get_modifier(pos)
-	if mod != null and is_instance_valid(mod):
-		var c = Cell.new()
-		c.neighbor_count = mod.neighbor_count
-		return grid.add_cell(pos, c)
-	return grid.add_cell(pos)
-
-func get_modifier(for_pos: Vector2) -> Variant:
-	if mod_map.has(for_pos):
-		return mod_map.get(for_pos)
-
-	var idx = modifiers.find_custom(func(mod):
-		var va = transform * Vector3(for_pos.x, 0, for_pos.y)
-		var vb = Vector3(mod.global_position.x, 0, mod.global_position.z)
-		return va.distance_to(vb) < 1.0
-	)
-	if idx != -1 and is_instance_valid(modifiers[idx]):
-		mod_map.set(for_pos, modifiers[idx])
-		return modifiers[idx]
-
-	mod_map.set(for_pos, null)
-	return null
-
-var timer = 0
 func _process(_dt):
-	if not Engine.is_editor_hint(): return
-	if draw_vertices:
-		var vs = grid.get_vertices()
-		for i in range(vs.size()):
-			if preview_cursor > -1 and i != preview_cursor: continue
-			draw_point(vs[i], Color.GREEN, str(i), Color.WHITE)
-		for pos in grid.cells.keys():
-			draw_polygon(grid.get_vertices(pos), Color.GREEN)
-			
-		var mod = get_modifier(Vector2.ZERO)
-		draw_point(Vector2(0,0), Color.RED if mod != null else Color.YELLOW, str(grid.get_vertices_idx(Vector2(0, 0))) )
+	DebugDraw3D.draw_aabb(grid_aabb, Color.YELLOW)
+	DebugDraw3D.draw_arrow(Vector3.ZERO, xz(grid.grid_to_world.x), Color.RED, .1)
+	DebugDraw3D.draw_arrow(Vector3.ZERO, xz(grid.grid_to_world.y), Color.GREEN, .1)
+	for x in range(-grid_size.x, grid_size.x + 1):
+		for y in range(-grid_size.y, grid_size.y + 1):
+			draw_vertices_2d(grid.get_vertices(Vector2(x,y)), Color.WHITE, str(x, ";", y))
+	for coord in modifiers.keys():
+		var mod = modifiers[coord]
+		DebugDraw3D.draw_sphere(mod.global_position, .01, Color.CYAN)
+		DebugDraw3D.draw_line(mod.global_position, xz(coord), Color.CYAN)
 
-	if not once:
-		once = true
-		_first_frame_duration = Time.get_ticks_msec() - _ts
+#### HELPERS
 
-		print("Generate time: %d ms, First frame time: %d ms, Total: %d ms" % [_generate_duration, _first_frame_duration, _generate_duration + _first_frame_duration])
+func draw_vertices_2d(vs: PackedVector2Array, color := Color.WHITE, text := ""):
+	var center = Vector3.ZERO
+	for i in range(vs.size()):
+		var a = xz(vs[i])
+		center += a
+		var b = xz(vs[(i+1)%vs.size()])
+		DebugDraw3D.draw_line(a, b, color)
+	if text != "":
+		center /= vs.size()
+		DebugDraw3D.draw_text(center + Vector3.UP, text, 32, color)
+
+func xz(v: Vector2) -> Vector3:
+	return transform * Vector3(v.x, 0, -v.y)
+
+func draw_point(pos: Vector2, color:= Color.WHITE, text := "", text_color := color, text_size := 32):
+	var wpos = transform * Vector3(pos.x, 0, pos.y)
+	DebugDraw3D.draw_sphere(wpos, .04, color)
+	if text != "":
+		DebugDraw3D.draw_text(wpos + Vector3.UP, text, text_size, text_color)
+
+####
 
 func _ready():
 	generate()
 
 func _notification(what):
-	if not Engine.is_editor_hint(): return
-	if what == NOTIFICATION_EDITOR_POST_SAVE or what == NOTIFICATION_WM_WINDOW_FOCUS_IN:
+	if what == NOTIFICATION_EDITOR_POST_SAVE:
 		generate()
 
-func draw_point(pos: Vector2, color := Color.WHITE, text = "", text_color = color, text_size := 32):
-	var world_pos = transform * Vector3(pos.x, 0, pos.y)
-	DebugDraw3D.draw_sphere(world_pos, 0.02, color)
-	if text != "":
-		DebugDraw3D.draw_text(world_pos + Vector3.UP * 2.0, text, text_size, text_color)
 
-func draw_polygon(vertices: PackedVector2Array, color := Color.WHITE, callable = null):
-	for i in range(vertices.size()):
-		var va = vertices[i]
-		var vb = vertices[(i+1)%vertices.size()]
-		var a = Vector3(va.x, 0, va.y)
-		var b = Vector3(vb.x, 0, vb.y)
-		if callable == null or not callable is Callable:
-			DebugDraw3D.draw_line(a, b, color)
-		else:
-			callable.call(a, b, color)
-
-class Grid extends RefCounted:
-	var base_cell: Cell
-	var cells: Dictionary[Vector2, Cell]
-
-	var neighbor_count: int:
-		set(v):
-			neighbor_count = v
-			base_cell = Cell.new()
-			base_cell.neighbor_count = v
-			_should_recompute_vertices = true
-
-	func get_cell(pos: Vector2) -> Cell:
-		var c = cells.get(pos)
-		if c != null:
-			return c
-		if base_cell == null:
-			base_cell = Cell.new(neighbor_count)
-		return base_cell
-
-	func get_default_neighbors(pos: Vector2) -> PackedVector2Array:
-		var arr = PackedVector2Array([])
-		var c = base_cell #get_cell(pos)
-		if c == null: return arr
-		for n in c.get_neighbors():
-			arr.append(n + pos)
-		return arr
-
-	func get_neighbors(pos: Vector2) -> PackedVector2Array:
-		var arr = PackedVector2Array([])
-		var c = get_cell(pos)
-		if c == null: return arr
-		for n in c.get_neighbors():
-			arr.append(n + pos)
-		return arr
-
-	func get_vertices(id = null) -> PackedVector2Array:
-		_compute_vertices()
-		var vertices = []
-		if id != null and id is Vector2:
-			var c = cells.get(id)
-			if c == null: c = base_cell
-			for v in c.get_vertices():
-				vertices.append(v + id)
-			return vertices
-
-		return _vertices
-
-	var _vertices: PackedVector2Array
-	var _merged_vertices: Dictionary[Vector2, int]
-	var _should_recompute_vertices := true
-	func _compute_vertices():
-		if not _should_recompute_vertices: return
-		_vertices.clear()
-		_merged_vertices.clear()
-
-		var vertices: Array = []
-		var needs_adjusting_vertices: Array = []
-		for pos in cells.keys():
-			var cell = cells[pos]
-			for v in cell.get_vertices():
-				var found = vertices.find_custom(func(it):
-					return it.distance_squared_to(pos + v) < 0.01
-				)
-				if found > -1:
-					_merged_vertices.set(v, found)
-				else:
-					vertices.append(pos + v)
-
-		for pos in cells.keys():
-			var c = cells[pos]
-			if c == base_cell: continue
-			#var adjacent_vertices = get_
-
-		_vertices = PackedVector2Array(vertices)
-		_should_recompute_vertices = false
-
-	func get_vertices_idx(pos: Vector2) -> Array:
-		_compute_vertices()
-		var indices = []
-		var c = get_cell(pos)
-		if c == null: c = base_cell
-		for v in c.get_vertices():
-			var remapped = _merged_vertices.get(pos + v)
-			if remapped != null:
-				indices.append(remapped)
-			else:
-				indices.append(_vertices.find(pos+v))
-
-		return indices
-
-	func get_vertices_in_range(pos: Vector2, distance: float) -> PackedVector2Array:
-		_compute_vertices()
-		return Array(_vertices).filter(func(v):
-			return v.distance_to(pos) <= distance
-		)
-
-	func add_cell(v: Vector2, cell_override = null) -> Cell:
-		if cells.has(v):
-			return cells.get(v)
-			
-		_should_recompute_vertices = true
-		if cell_override == null or not cell_override is Cell:
-			if base_cell == null:
-				base_cell = Cell.new(neighbor_count)
-			cells.set(v, base_cell)
-			return base_cell
-
-		cells.set(v, cell_override)
-		return cell_override
-
-	func clear():
-		_should_recompute_vertices = true
-		cells.clear()
-
-class Cell extends RefCounted:
-	var neighbor_count := 6
-
-	func _init(neighbor_count := 6):
-		neighbor_count = neighbor_count
-
-	func get_vertices() -> PackedVector2Array:
-		var verts = []
-		var angle = (PI * 2.0) / neighbor_count
-		var base_direction_vector = Vector2.UP
-		if neighbor_count == 4:
-			base_direction_vector = Vector2(1,1).normalized()
-		for i in range(neighbor_count):
-			verts.append(base_direction_vector.rotated(angle * i))
-		return verts
-
-	func get_neighbors() -> PackedVector2Array:
-		var verts = []
-		var angle = (PI * 2.0) / neighbor_count
-		var base_direction_vector = Vector2.UP
-		var neighbor_dist = cos(angle / 2.0) * 2.0
-		if neighbor_count == 4:
-			base_direction_vector = Vector2(1,1).normalized()
-		base_direction_vector = base_direction_vector.rotated(angle / 2.0).normalized() * neighbor_dist
-		for i in range(neighbor_count):
-			verts.append(base_direction_vector.rotated(angle * i))
-		return verts
+		
--- battle/graph_modifier.gd
+++ battle/graph_modifier.gd
@@ -2,7 +2,7 @@
 extends Node3D
 class_name GraphModifier
 
-@export var neighbor_count := 6
+@export var neighbors := 6
 @export var affect_position := false
 
 var cell_coordinate: Vector2
